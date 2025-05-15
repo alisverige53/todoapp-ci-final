92888b406c1cc1399b6cee401d6fb0c1f659edb2
ali asgari <alisverige53@gmail.com>
2025-05-15 20:25:20 +0200

    fix: include test resources for CI

--- src/test/java/com/example/todoapp/integration/TaskIntegrationTest.java
+++ src/test/java/com/example/todoapp/integration/TaskIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -20,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-
+@TestPropertySource(locations = "classpath:application-test.properties")
 @ActiveProfiles("test")
 @SpringBootTest(classes = TodoappApplication.class) //för todoapp
 @AutoConfigureMockMvc
