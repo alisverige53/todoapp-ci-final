a242a8611da8cd6d5559a298e83e2b9d99380293
ali asgari <alisverige53@gmail.com>
2025-05-15 22:01:57 +0200

    Remove wrongly duplicated todoapp folder from Desktop

--- Desktop/todoapp/src/main/java/com/example/todoapp/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todoapp.repository;
-
-import com.example.todoapp.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
